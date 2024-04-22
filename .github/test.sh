a70f3fcafbbb815ae92cc5fcb85ad0c817a8a26e
fsologureng <fsologur@uchile.cl>
2024-04-22 14:03:10 -0400

    Workflow: centralize external test workflow

--- .github/test.sh
+++ .github/test.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-pip install -e /openedx/requirements/eol_sso_login
+pip install -e /openedx/requirements/app
 
-cd /openedx/requirements/eol_sso_login
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eol_sso_login
+cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eol_sso_login/tests.py
-
-rm -rf test_root
